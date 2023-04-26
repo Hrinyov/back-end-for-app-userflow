91300797b4db980c9dfed689c8a534f28b4012f4
Vlad <hrinyov@gmail.com>
2023-04-27 00:24:55 +0300

    prisma init

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 import express from 'express'
-import { getUser, getUsers, createUser} from './database.js';
-import { getEvent, getEvents, createEvent } from './database.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
@@ -10,41 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-
-app.get('/users', async (req, res) => {
-    const users = await getUsers();
-    res.send(users);
-})
-
-app.get('/users/:id', async (req, res) => {
-    const id = req.params.id;
-    const user = await getUser(id);
-    res.send(user);
-})
-
-app.post('/users', async (req, res) => {
-    const { username, email, phonenumber } =req.body;
-    const user = await createUser(username, email, phonenumber);
-    res.status(201).send(user);
-})
-
-app.get('/events', async (req, res) => {
-    const events = await getEvents();
-    res.send(events);
-})
-
-app.get('/events/:id', async (req, res) => {
-    const id = req.params.id;
-    const event = await getEvent(id);
-    res.send(event);
-})
-
-app.post('/events', async (req, res) => {
-    const { title, description, startDate, endDate, userId } = req.body;
-    const event = await createEvent(title, description, startDate, endDate, userId);
-    res.status(201).send(event);
-})
 
 
 app.use((err, req, res, next) => {
