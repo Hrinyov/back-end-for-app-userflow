b6e3111966ebb72a616fcfddc5fcb913008e8681
Vlad <hrinyov@gmail.com>
2023-04-30 21:01:40 +0300

    continued to write requests methods for events

--- app.js
+++ app.js
@@ -91,13 +91,45 @@ app.get('/events/:userId', async (req, res) => {
 })
 
 app.post('/events', async (req, res) => {
-    const { username, email } = req.body;
-    
+    const { title, description, startDate, endDate, userId } = req.body;
     try {
-        const event = await prisma.event.create({
-            data: req.body
-        })
-        res.sendStatus(201)    
+        const existingEvents = await prisma.event.findMany({
+            where: {
+                userId: Number(userId),
+                OR: [
+                    {
+                        startDate: {
+                            lte: new Date(endDate)
+                        },
+                        endDate: {
+                            gte: new Date(startDate)
+                        }
+                    },
+                    {
+                        startDate: {
+                            gte: new Date(startDate)
+                        },
+                        endDate: {
+                            lte:    new Date(endDate) 
+                        }
+                    }
+                ]
+            }
+        });
+        if (existingEvents.length > 0){
+            res.sendStatus(409)
+        } else {
+            const event = await prisma.event.create({
+                data: {
+                    title: title,
+                    description: description,
+                    startDate: new Date(startDate),
+                    endDate: new Date(endDate),
+                    userId: Number(userId)
+                }
+            })
+        res.sendStatus(201)     
+        }     
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
