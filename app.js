7d8e28afbcf1993b8da4f29afdd6b729c7f62f5a
Vlad <hrinyov@gmail.com>
2023-05-07 23:33:56 +0300

    add new request method and fix old

--- app.js
+++ app.js
@@ -2,15 +2,44 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+import passport from './passport.js';
+import jwt from 'jsonwebtoken'
+import session from 'express-session';
+import bcrypt from 'bcryptjs'
 
 const app = express();
-
-
 const prisma = new PrismaClient();
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     );
+//     if (req.method === 'OPTIONS') {
+//         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//         return res.status(200).json({});
+//     }
+//     next();
+// });
 
 
 app.get('/users/:id', async (req, res) => {
@@ -19,7 +48,14 @@ app.get('/users/:id', async (req, res) => {
        const user = await prisma.user.findUnique({
         where:{
             id: Number(id)
-        }
+        },
+        select: {
+           id: true,
+           username: true,
+           email: true,
+           phonenumber: true,
+           events: true,
+        },
        })
        res.json(user)
     } catch (error) {
@@ -31,7 +67,13 @@ app.get('/users/:id', async (req, res) => {
 app.get('/users', async (req, res) => {
     try {
         const users = await prisma.user.findMany({
-            include: { events: true }
+            select: {
+                id: true,
+                username: true,
+                email: true,
+                phonenumber: true,
+                events: true,
+            },
         })
         res.json(users)
     } catch (error) {
@@ -42,9 +84,9 @@ app.get('/users', async (req, res) => {
 
 
 app.post('/users', async (req, res) => {
-    const { username } = req.body;
+    const { username, password } = req.body;
     try {
-        const existingUser = await prisma.user.findMany({
+        const existingUser = await prisma.user.findFirst({
             where: {
                 username: username
             }
@@ -52,10 +94,18 @@ app.post('/users', async (req, res) => {
         if (existingUser) {
             res.sendStatus(409)
         } else {
-            const event = await prisma.user.create({
-                data: req.body
-            })
-            res.sendStatus(201)
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const user = await prisma.user.create({
+                data: {
+                    username: username,
+                    password: hashedPassword,
+                    email: req.body.email,
+                    phonenumber: req.body.phonenumber,
+                },
+            });
+
+            res.sendStatus(201);
         }
     } catch (error) {
         console.error(error);
@@ -135,6 +185,26 @@ app.post('/events', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 })
+
+
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({ message: info.message });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+
+        return res.json({ token });
+    })(req, res, next);
+});
+
+
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
